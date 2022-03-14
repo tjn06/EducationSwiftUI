f375d1335c33d1995416dba1adf4f31714f42a17
Tobias Johnsson <tobb-on@hotmail.com>
2022-03-14 18:40:39 +0100

    Initial Commit

---
+++ Education/EducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationApp.swift
+//  Education
+//
+//  Created by Tobias Johnsson on 2022-03-14.
+//
+
+import SwiftUI
+
+@main
+struct EducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
